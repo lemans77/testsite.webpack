11b1b3484356bf610b3064c32cfa1eea8ba3b104
lemans77 <oleg.boiechko@gmail.com>
2022-07-23 16:33:19 +0300

    rename variables and function

--- src/js/ejs_variables.js
+++ src/js/ejs_variables.js
@@ -1,4 +1,4 @@
-let ul = [
+let faq = [
   {
     name: 'Is Foodies Bread really baked fresh each day?',
     text: 'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language.'
@@ -17,4 +17,4 @@ let ul = [
   }
 ];
 
-export { ul }
+export { faq }
