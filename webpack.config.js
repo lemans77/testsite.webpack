674ff27bea1f3ba4861e88eb1b414aef40c12563
lemans77 <oleg.boiechko@gmail.com>
2022-07-23 14:45:04 +0300

    install sccs module and add to project

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 let mode = 'development'
 if (process.env.NODE_ENV === 'production') {
@@ -28,11 +29,36 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css'
     })
   ],
   module: {
     rules: [
-
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          (mode === 'development') ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  [
+                    'postcss-preset-env',
+                    {
+                        // Options
+                    },
+                  ],
+                ],
+              },
+            },
+          },
+          'sass-loader',
+        ],
+      }
     ]
   }
 }
