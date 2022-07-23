3e00974128cbdb3449aa7e903e2cdd8ac739efa0
lemans77 <oleg.boiechko@gmail.com>
2022-07-23 16:26:43 +0300

    add section sectionFAQ

---
+++ src/js/ejs_variables.js
@@ -0,0 +1,20 @@
+let ul = [
+  {
+    name: 'Is Foodies Bread really baked fresh each day?',
+    text: 'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language.'
+  },
+  {
+    name: 'Do you bake breads containing animal fats or products?',
+    text: 'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language.'
+  },
+  {
+    name: 'Can I order your products online?',
+    text: 'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language.'
+  },
+  {
+    name: 'When are you opening a shop near me?',
+    text: 'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language.'
+  }
+];
+
+export { ul }
