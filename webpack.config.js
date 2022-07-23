05dfd59cc6405f7dfb5dc600fea28b5b2f28fa63
lemans77 <oleg.boiechko@gmail.com>
2022-07-23 14:30:34 +0300

    add script dev and build

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,11 @@
+let mode = 'development'
+if (process.env.NODE_ENV === 'production') {
+  mode = 'production'
+}
+console.log(mode + ' mode')
+
 module.exports = {
+  mode: mode,
   entry: {
     index: './src/js/index.js'
   }
