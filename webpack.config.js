ecba8ffa27307db3ec68a52d4cb677f77667d348
lemans77 <oleg.boiechko@gmail.com>
2022-07-23 14:32:50 +0300

    install webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 let mode = 'development'
 if (process.env.NODE_ENV === 'production') {
   mode = 'production'
@@ -8,5 +10,13 @@ module.exports = {
   mode: mode,
   entry: {
     index: './src/js/index.js'
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000,
+    open: true
   }
 }
